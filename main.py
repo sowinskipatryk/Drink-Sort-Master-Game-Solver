6b23c378edc31ddc22e898cf0305150d13c48c06
sowinskipatryk <91700001+sowinskipatryk@users.noreply.github.com>
2022-09-24 00:26:00 +0200

    clean version

--- main.py
+++ main.py
@@ -6,11 +6,20 @@ class Bottle:
         self.capacity = 4
         self.colors = [arg for arg in args]
 
-    def is_sorted(self):  # checks if the bottle is sorted (four segments in one color or empty)
-        return (len(set(self.colors)) == 1 and len(self.colors) == self.capacity) or not self.colors
+    def is_empty(self):
+        return not self.colors
 
-    def count_same_colors(self):  # counts how many layers of the same color are in a specific vessel
-        if not self.colors:
+    def is_full(self):
+        return len(self.colors) == self.capacity
+
+    def is_single_colored(self):
+        return len(set(self.colors)) == 1
+
+    def is_sorted(self):
+        return (self.is_single_colored() and self.is_full()) or self.is_empty()
+
+    def count_layers(self):  # how many layers of the same color are in a specific vessel
+        if self.is_empty():
             return 0
 
         counter = 1
@@ -29,9 +38,18 @@ class Game:
         self.bottle_to = None
         self.recursion_level = 1
         self.moves_history = []
+        self.walkthrough = []
 
-    def add(self, bottle):
+    def add_bottle(self, bottle):
         self.bottles.append(bottle)
+
+    def check_data_input(self):
+        color_list = []
+        for bottle in self.bottles:
+            color_list += bottle.colors
+        color_counter = Counter(color_list)
+        print('Input check:', color_counter)
+        return all(color_counter[each] == 4 or color_counter[each] == 0 for each in color_counter)
 
     def status(self, step_hint=True):
         print()
@@ -44,36 +62,28 @@ class Game:
                 print(i+1, bottle.colors)
         print()
 
-    def check_data_input(self):
-        color_list = []
-        for b in self.bottles:
-            color_list += b.colors
-        counter = Counter(color_list)
-        print('Input check:', counter)
-        return all(counter[each] == 4 or counter[each] == 0 for each in counter)
-
     def all_drinks_sorted(self):
-        return all(b.is_sorted() for b in self.bottles)
+        return all(bottle.is_sorted() for bottle in self.bottles)
 
-    def move(self, x, y):
-        if x.colors:  # if the vessel is not empty
-            if x is not y:  # if it's different from the one to pour into
-                if not y.colors or (x.colors[-1] == y.colors[-1]):  # if the vessel to pour into is empty or the same color
-                    if not x.is_sorted():  # if the vessel to pour from is not sorted yet
-                        if not (len(set(x.colors)) == 1 and not y.colors):  # prevent from moving color to an empty vessel is it's sorted but not yet full
-                            space = y.capacity - len(y.colors)
-                            if space:  # if there is room for color to be poured
-                                if (y, x, x.colors[-1]) not in self.moves_history:  # prevent from moving one color between two vessels
+    def move(self, b, c):
+        if b.colors:  # if the vessel is not empty
+            if b is not c:  # if it's different from the one to pour into
+                if c.is_empty() or (b.colors[-1] == c.colors[-1]):  # if the vessel to pour into is empty or the same color
+                    if not b.is_sorted():  # if the vessel to pour from is not sorted yet
+                        if not (b.is_single_colored() and c.is_empty()):  # prevent from moving color to an empty vessel is it's sorted but not yet full
+                            room_available = c.capacity - len(c.colors)
+                            if room_available:  # if there is room for color to be poured
+                                if (c, b, b.colors[-1]) not in self.moves_history:  # prevent from moving one color between two vessels
                                     print('IN', self.recursion_level)
                                     self.recursion_level += 1
-                                    self.bottle_from = x
-                                    self.bottle_to = y
+                                    self.bottle_from = b
+                                    self.bottle_to = c
                                     self.status()
-                                    limit = min(space, x.count_same_colors())
-                                    for _ in range(limit):  # for a number of colors
-                                        y.colors.append(x.colors.pop())  # append to the second vessel the color we take from first vessel
-                                    self.moves_history.append((x, y, y.colors[-1]))
-                                    return limit
+                                    layers = min(room_available, b.count_layers())
+                                    for _ in range(layers):  # for a number of colors
+                                        c.colors.append(b.colors.pop())  # append to the second vessel the color we take from first vessel
+                                    self.moves_history.append((b, c, c.colors[-1]))
+                                    return layers
 
     def sort(self):
         if self.all_drinks_sorted():
@@ -109,19 +119,19 @@ def game_solver(game):
 
 game = Game()
 
-game.add(Bottle('mint', 'lightblue', 'lightblue'))
-game.add(Bottle('yellow', 'gray', 'pink', 'pink'))
-game.add(Bottle('gray', 'violet', 'blue', 'blue'))
-game.add(Bottle('purple', 'yellow', 'blue', 'skin'))
-game.add(Bottle('lime', 'purple', 'lightblue', 'skin'))
-game.add(Bottle('purple', 'purple'))
-game.add(Bottle('violet', 'skin', 'lightblue', 'lime'))
-game.add(Bottle('gray', 'violet', 'pink'))
-game.add(Bottle('mint', 'violet', 'lime', 'mint'))
-game.add(Bottle('lazur', 'red', 'yellow'))
-game.add(Bottle('yellow'))
-game.add(Bottle('skin', 'mint', 'blue', 'gray'))
-game.add(Bottle('lime', 'red', 'lazur', 'lazur'))
-game.add(Bottle('lazur', 'pink', 'red', 'red'))
+game.add_bottle(Bottle('mint', 'lightblue', 'lightblue'))
+game.add_bottle(Bottle('yellow', 'gray', 'pink', 'pink'))
+game.add_bottle(Bottle('gray', 'violet', 'blue', 'blue'))
+game.add_bottle(Bottle('purple', 'yellow', 'blue', 'skin'))
+game.add_bottle(Bottle('lime', 'purple', 'lightblue', 'skin'))
+game.add_bottle(Bottle('purple', 'purple'))
+game.add_bottle(Bottle('violet', 'skin', 'lightblue', 'lime'))
+game.add_bottle(Bottle('gray', 'violet', 'pink'))
+game.add_bottle(Bottle('mint', 'violet', 'lime', 'mint'))
+game.add_bottle(Bottle('lazur', 'red', 'yellow'))
+game.add_bottle(Bottle('yellow'))
+game.add_bottle(Bottle('skin', 'mint', 'blue', 'gray'))
+game.add_bottle(Bottle('lime', 'red', 'lazur', 'lazur'))
+game.add_bottle(Bottle('lazur', 'pink', 'red', 'red'))
 
 game_solver(game)
